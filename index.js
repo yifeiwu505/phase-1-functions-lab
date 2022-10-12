d8ae75daabe6221cc99cfabcc60365523df3f91f
yifeiwu <wyf990505@gmail.com>
2022-10-12 16:31:32 -0400

    commit tree 66fe28ec16fa7d8c1e8064e97030fb7e3faa0723

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue>=42){
+       return someValue-42;
+    } 
+    else{
+        
+        return 42-someValue;
+         
+    }
+    
+}
+
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+ 
+}
+function distanceTravelledInFeet(start, destination){
+    if (destination>start){
+        return (destination-start)*264;
+    }
+    else{
+        return (start-destination)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance<400){
+        return 0;
+    }
+    else if (distance>=400 && distance<=2000){
+        return (distance-400)*0.02;
+    }
+    else if (distance>=2000 && distance< 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far'
+    }
+}
